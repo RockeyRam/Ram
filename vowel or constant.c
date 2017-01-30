5922685f7e129f2ce937f4b2175d1b281ba2b586
RockeyRam <rockeyramsaran@gmail.com>
2017-01-30 07:38:19 -0800

    Update vowel or constant.c

--- vowel or constant.c
+++ vowel or constant.c
@@ -1,18 +1,16 @@
 #include<stdio.h>
-#include<conio.h>
-void main()
+int main()
 {
-  char a;
-  clrscr();
-  printf("\n Enter the vlue");
-  scanf("%c",a);
-  if(a==('a'||'A')||a==('e'||'E')||a==('i'||'I')||a==('O'||'o')||a==('u'||'U'))
+  char aa;
+  printf("\n Enter the value");
+  scanf("%c",&aa);
+  if(aa=='a'||'A'||'e'||'I'||'i'||'o'||'O'||'u'||'U')
   {
-    printf("\n The given value is vowel);
+    printf("\n The given value is vowel");
    }
    else
    {
       printf("\n The given value is constant");
-    }
-    getch();
+   }
+    return 0;
 }
